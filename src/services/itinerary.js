6a3b832999fdfc067f082575e9e3b7d0600047b6
premlata15 <chaudharymuskan94@gmail.com>
2026-02-06 16:49:35 +0545

    again changed

--- src/services/itinerary.js
+++ src/services/itinerary.js
@@ -1,6 +1,6 @@
 import Itinerary from "../models/itinerary.js";
 import NotFoundError from "../errors/not-found-error.js";
-import ValidationError from "../errors/Validation.js";
+import ValidationError from "../errors/validation.js";
 import { getTripById } from "./trips.js";
 
 export const createItinerary = async (itineraryData) => {
